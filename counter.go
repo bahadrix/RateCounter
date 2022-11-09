0012876dbecc4959ae93f8c426f3e50a8f957f28
Bahadir <bahadrix@gmail.com>
2022-11-09 18:26:44 +0300

    structural fixes

--- counter.go
+++ counter.go
@@ -6,41 +6,41 @@ import (
 	"time"
 )
 
-type Bucket struct {
+type timeBucket struct {
 	counter       uint
 	firstHitStamp int64
 }
 
-type RateCounter struct {
+type Counter struct {
 	window      time.Duration
 	tickLength  time.Duration
-	buckets     []Bucket
+	buckets     []timeBucket
 	mx          sync.Mutex
 	windowStart int64
 }
 
-func NewRateCounter(window time.Duration, bucketSize time.Duration) (rc *RateCounter, err error) {
+func NewRateCounter(window time.Duration, bucketSize time.Duration) (rc *Counter, err error) {
 
 	if bucketSize > window {
 		err = errors.New("bucket size must be smaller than window")
 		return
 	}
-	rc = &RateCounter{
+	rc = &Counter{
 		window:     window,
 		tickLength: bucketSize,
-		buckets:    make([]Bucket, window/bucketSize),
+		buckets:    make([]timeBucket, window/bucketSize),
 	}
 	return
 }
 
-func (rc *RateCounter) resetBuckets() {
+func (rc *Counter) resetBuckets() {
 	for i := 0; i < len(rc.buckets); i++ {
 		rc.buckets[i].counter = 0
 		rc.buckets[i].firstHitStamp = 0
 	}
 }
 
-func (rc *RateCounter) Hit() {
+func (rc *Counter) Hit() {
 	stamp := time.Now().UnixNano()
 	rc.mx.Lock()
 	defer rc.mx.Unlock()
@@ -54,10 +54,9 @@ func (rc *RateCounter) Hit() {
 	if bucket.firstHitStamp == 0 {
 		bucket.firstHitStamp = stamp
 	}
-	//fmt.Printf("hit: %v\n", rc.buckets)
 }
 
-func (rc *RateCounter) Get() float64 {
+func (rc *Counter) Get() float64 {
 	rc.mx.Lock()
 	defer rc.mx.Unlock()
 
@@ -78,7 +77,7 @@ func (rc *RateCounter) Get() float64 {
 	return float64(sum) / passedSeconds
 }
 
-func (rc *RateCounter) index(stamp int64) int {
+func (rc *Counter) index(stamp int64) int {
 	i := int((stamp - rc.windowStart) / rc.tickLength.Nanoseconds())
 	if i >= len(rc.buckets) {
 		steps := i - len(rc.buckets) + 1
@@ -95,12 +94,12 @@ func (rc *RateCounter) index(stamp int64) int {
 	return i
 }
 
-func (rc *RateCounter) shift() {
+func (rc *Counter) shift() {
 	var i int
 	for i = 1; i < len(rc.buckets); i++ {
 		rc.buckets[i-1] = rc.buckets[i]
 	}
-	rc.buckets[i-1] = Bucket{} // i - 1 -> last element
+	rc.buckets[i-1] = timeBucket{} // i - 1 -> last element
 	if rc.buckets[0].firstHitStamp != 0 {
 		rc.windowStart = rc.buckets[0].firstHitStamp
 	}
